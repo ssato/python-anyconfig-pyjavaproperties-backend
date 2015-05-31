387ee9254f253da3530262dd0ba87a693e95f6f3
Satoru SATOH <satoru.satoh@gmail.com>
2015-05-31 16:53:52 +0900

    some cleanups

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup, Command, find_packages
 
 import datetime
-import glob
 import os
-import sys
 
-curdir = os.getcwd()
-sys.path.append(curdir)
 
 PACKAGE = "anyconfig-pyjavaproperties-backend"
 VERSION = "0.0.2"
@@ -16,20 +12,14 @@ if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
     import datetime
     VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
 
-data_files = []
-
 
 class SrpmCommand(Command):
 
     user_options = []
-
     build_stage = "s"
-    cmd_fmt = """rpmbuild -b%(build_stage)s \
-        --define \"_topdir %(rpmdir)s\" \
-        --define \"_sourcedir %(rpmdir)s\" \
-        --define \"_buildroot %(BUILDROOT)s\" \
-        %(rpmspec)s
-    """
+
+    curdir = os.path.abspath(os.curdir)
+    rpmspec = os.path.join(curdir, "pkg/package.spec")
 
     def initialize_options(self):
         pass
@@ -38,30 +28,21 @@ class SrpmCommand(Command):
         pass
 
     def run(self):
+        self.pre_sdist()
         self.run_command('sdist')
         self.build_rpm()
 
+    def pre_sdist(self):
+        c = open(self.rpmspec + ".in").read()
+        open(self.rpmspec, "w").write(c.replace("@VERSION@", VERSION))
+
     def build_rpm(self):
-        params = dict()
+        rpmbuild = os.path.join(self.curdir, "pkg/rpmbuild-wrapper.sh")
+        workdir = os.path.join(self.curdir, "dist")
 
-        params["build_stage"] = self.build_stage
-        rpmdir = params["rpmdir"] = os.path.join(
-            os.path.abspath(os.curdir), "dist"
-        )
-        rpmspec = params["rpmspec"] = os.path.join(
-            rpmdir, "../python-%s.spec" % PACKAGE
-        )
-
-        for subdir in ("SRPMS", "RPMS", "BUILD", "BUILDROOT"):
-            sdir = params[subdir] = os.path.join(rpmdir, subdir)
-
-            if not os.path.exists(sdir):
-                os.makedirs(sdir, 493)  # 493 = 0o755 (py3) or 0755 (py2)
-
-        c = open(rpmspec + ".in").read()
-        open(rpmspec, "w").write(c.replace("@VERSION@", VERSION))
-
-        os.system(self.cmd_fmt % params)
+        cmd_s = "%s -w %s -s %s %s" % (rpmbuild, workdir, self.build_stage,
+                                       self.rpmspec)
+        subprocess.check_call(cmd_s, shell=True)
 
 
 class RpmCommand(SrpmCommand):
@@ -69,40 +50,39 @@ class RpmCommand(SrpmCommand):
     build_stage = "b"
 
 
+_CLASSIFIERS = ["Development Status :: 4 - Beta",
+                "Intended Audience :: Developers",
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 2",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3.3",
+                "Programming Language :: Python :: 3.4",
+                "Operating System :: OS Independent",
+                "Topic :: Software Development :: Libraries :: Python Modules",
+                "Topic :: Text Processing :: Markup",
+                "Topic :: Utilities",
+                "License :: OSI Approved :: MIT License"]
+
+
+TESTS_REQ = [l.rstrip() for l in open("pkg/test_requirements.txt").readlines()
+             if l and not l.startswith('#')]
+
 setup(name=PACKAGE,
-    version=VERSION,
-    description="Generic access to configuration files in some formats",
-    long_description=open("README.rst").read(),
-    author="Satoru SATOH",
-    author_email="ssato@redhat.com",
-    license="MIT",
-    url="https://github.com/ssato/python-anyconfig-pyjavaproperties-backend",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Environment :: Console",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: Markup",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-    ],
-    tests_require=['nose>=1.0', 'pep8'],
-    #packages=find_packages(),
-    packages=[
-        "anyconfig.backend",
-        "anyconfig.backend.tests",
-    ],
-    include_package_data=True,
-    cmdclass={
-        "srpm": SrpmCommand,
-        "rpm":  RpmCommand,
-    },
-    entry_points=open(os.path.join(curdir, "aux/entry_points.txt")).read(),
+      version=VERSION,
+      description="Generic access to configuration files in some formats",
+      long_description=open("README.rst").read(),
+      author="Satoru SATOH",
+      author_email="ssato@redhat.com",
+      license="MIT",
+      url="https://github.com/ssato/python-anyconfig-pyjavaproperties-backend",
+      classifiers=_CLASSIFIERS,
+      tests_require=TESTS_REQ,
+      packages=find_packages(),
+      include_package_data=True,
+      cmdclass=dict(srpm=SrpmCommand, rpm=RpmCommand),
+      entry_points=open(os.path.join(curdir, "pkg/entry_points.txt")).read(),
 )
 
 # vim:sw=4:ts=4:et:
